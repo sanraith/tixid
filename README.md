f5a3a2d6f10980c928829ca1a8f111d0a557fdcd
Soma Zsják <zsjaks@gmail.com>
2020-06-02 21:32:44 +0200

    Added types, nodemon and better startup script.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# qind
+# QIND
+
 QIND is not Dixit.
+
+## Scripts
+
+- **start:** Builds and starts the application.
+- **start-dev:** Runs the application in dev mode. Automatically restarts on changes in the *src* directory.
+- **update-types:** Adds the missing type definitions as dependencies.
--- src/start.ts
+++ src/start.ts
@@ -4,22 +4,23 @@
  * Module dependencies.
  */
 
-var app = require('../dist/app');
-var debug = require('debug')('qind:server');
-var http = require('http');
+import app from './app';
+import http from 'http';
+import _debug from 'debug';
+const debug = _debug('qind:server');
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 /**
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
+const server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
@@ -33,8 +34,8 @@ server.on('listening', onListening);
  * Normalize a port into a number, string, or false.
  */
 
-function normalizePort(val) {
-  var port = parseInt(val, 10);
+function normalizePort(val: any) {
+  const port = parseInt(val, 10);
 
   if (isNaN(port)) {
     // named pipe
@@ -53,12 +54,12 @@ function normalizePort(val) {
  * Event listener for HTTP server "error" event.
  */
 
-function onError(error) {
+function onError(error: any) {
   if (error.syscall !== 'listen') {
     throw error;
   }
 
-  var bind = typeof port === 'string'
+  const bind = typeof port === 'string'
     ? 'Pipe ' + port
     : 'Port ' + port;
 
@@ -82,9 +83,9 @@ function onError(error) {
  */
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
+  const addr = server.address();
+  const bind = typeof addr === 'string'
     ? 'pipe ' + addr
-    : 'port ' + addr.port;
+    : 'port ' + addr?.port;
   debug('Listening on ' + bind);
 }
