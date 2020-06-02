dd4d915f28aeb66b965e177c6d73008dead10c93
Soma Zsják <zsjaks@gmail.com>
2020-06-02 20:31:29 +0200

    Added routes, views + advanced ts compile script.

---
+++ build.js
@@ -0,0 +1,17 @@
+const fs = require('fs-extra');
+const childProcess = require('child_process');
+
+
+try {
+    // Remove current build
+    fs.removeSync('./dist/');
+
+    // Copy front-end files
+    fs.copySync('./src/public', './dist/public');
+    fs.copySync('./src/views', './dist/views');
+
+    // Transpile the typescript files
+    childProcess.exec('npx tsc');
+} catch (err) {
+    console.log(err);
+}
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,49 @@
+import createError from 'http-errors';
 import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import sassMiddleware from 'node-sass-middleware';
 
-const app = express();
-const port = 3000;
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
 
-app.get('/', (req, res) => {
-  res.send('The app is running!');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true, // true = .sass and false = .scss
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.listen(port, err => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`server is listening on ${port}`);
+// error handler
+app.use(function(err: any, req: any, res: any, next: any) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+// module.exports = app;
+export default app;
