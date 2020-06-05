30ca68cee0f799e66939fa9ea7c6306cc9f1090b
Soma Zsják <zsjaks@gmail.com>
2020-06-05 19:14:43 +0200

    Moved build script. Turned on logs by default.

--- util/build.js
+++ util/build.js
@@ -1,8 +1,11 @@
+const path = require('path');
 const fs = require('fs-extra');
 const childProcess = require('child_process');
 
-
 try {
+    // Go to root folder
+    process.chdir(path.join(__dirname, '..'));
+
     // Remove current build
     fs.removeSync('./dist/');
 
@@ -14,4 +17,6 @@ try {
     childProcess.exec('npx tsc');
 } catch (err) {
     console.log(err);
+} finally {
+    process.chdir(__dirname);
 }
