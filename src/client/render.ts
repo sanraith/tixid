7c4e942836f7fbeb751b652de5c6271980e9607f
Soma Zsják <zsjaks@gmail.com>
2020-06-14 21:43:15 +0200

    Added empty ng project.

--- src/client/render.ts
+++
@@ -1,8 +0,0 @@
-class Renderer {
-    playerList(players: { name: string }[]) {
-        const playerList = document.getElementById("playerList");
-        playerList.innerHTML = players.map(x => x.name).join(", ");
-    }
-}
-
-var render = new Renderer();
