3d710a6a35588555188aae6ec154d4ae825d98af
Soma Zsják <zsjaks@gmail.com>
2020-09-08 21:58:45 +0200

    Enable CardPicker to select multiple cards.

--- src/app/room/card-picker/card-picker.component.ts
+++ src/app/room/card-picker/card-picker.component.ts
@@ -1,50 +1,86 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-card-picker',
-  templateUrl: './card-picker.component.html',
-  styleUrls: ['./card-picker.component.sass']
+    selector: 'app-card-picker',
+    templateUrl: './card-picker.component.html',
+    styleUrls: ['./card-picker.component.sass']
 })
 export class CardPickerComponent implements OnInit {
-  @Input() cards: string[];
-  @Input() selectedCard: string;
-  @Input() isEnabled: boolean = true;
-  @Input() isOverlayIconEnabled: boolean = true;
-  @Input() blockedCards: string[] = [];
-  @Input() imageHeightVh: number = 20;
-  @Output() selectedCardChange = new EventEmitter();
+    // Data
+    @Input() cards: string[];
+    @Input() selectedCard: string;
+    @Input() selectedCards: string[] = [];
+    @Input() blockedCards: string[] = [];
+    @Output() selectedCardChange = new EventEmitter();
+    @Output() selectedCardsChange = new EventEmitter();
 
-  displayedCard: string;
+    // Behavior
+    @Input() isEnabled: boolean = true;
+    @Input() maxSelectedCardCount: number = 1;
+    @Input() isOverlayIconEnabled: boolean = true;
 
-  constructor() { }
+    // Style
+    @Input() imageHeightVh: number = 20;
 
-  ngOnInit(): void {
-  }
+    displayedCard: string;
 
-  _selectedCardChange(): void {
-    this.selectedCardChange.emit(this.selectedCard);
-  }
-
-  selectCard(card: string): void {
-    if (this.isEnabled && !this.blockedCards.includes(card)) {
-      this.selectedCard = card;
-      this._selectedCardChange();
+    ngOnInit(): void {
+        if (this.selectedCard) {
+            this.selectedCards = [this.selectedCard];
+        }
     }
-  }
 
-  displayCard(card?: string): void {
-    this.displayedCard = card;
-  }
+    selectCard(card: string, isSelected: boolean): void {
+        // Do not allow changing state of blocked cards or in disabled mode.
+        if (!this.isEnabled || this.blockedCards.includes(card)) {
+            return;
+        }
 
-  changeDisplayedCard(direction: number): void {
-    const index = this.cards.indexOf(this.displayedCard);
-    if (index === -1) { return; }
+        // Add or remove card from selected cards.
+        if (isSelected) {
+            if (!this.selectedCards.includes(card)) {
+                this.selectedCards.push(card);
+            }
+        } else {
+            const index = this.selectedCards.indexOf(card);
+            if (index === -1) {
+                return;
+            }
+            this.selectedCards.splice(index, 1);
+        }
 
-    const newIndex = Math.max(0, Math.min(this.cards.length - 1, index + direction));
-    this.displayedCard = this.cards[newIndex];
-  }
+        // Remove oldest selected card if more are selected than the maximum amount.
+        if (this.selectedCards.length > this.maxSelectedCardCount) {
+            this.selectedCards.splice(0, 1);
+        }
 
-  onWheelEvent(scrollEventArgs: { deltaY: number }) {
-    this.changeDisplayedCard(scrollEventArgs.deltaY > 0 ? 1 : -1);
-  }
+        this.fireSelectedCardsChangeEvents();
+    }
+
+    fireSelectedCardsChangeEvents(): void {
+        if (this.selectedCards.length < 2) {
+            this.selectedCard = this.selectedCards[0] ?? null;
+        } else {
+            this.selectedCard = null;
+        }
+        this.selectedCardChange.emit(this.selectedCard);
+        this.selectedCardsChange.emit(this.selectedCards);
+    }
+
+    displayCard(card?: string): void {
+        this.displayedCard = card;
+    }
+
+    changeDisplayedCard(direction: number): void {
+        const index = this.cards.indexOf(this.displayedCard);
+        if (index === -1) { return; }
+
+        const newIndex = Math.max(0, Math.min(this.cards.length - 1, index + direction));
+        this.displayedCard = this.cards[newIndex];
+    }
+
+    onWheelEvent(scrollEventArgs: { preventDefault: () => void, deltaY: number }) {
+        scrollEventArgs.preventDefault();
+        this.changeDisplayedCard(scrollEventArgs.deltaY > 0 ? 1 : -1);
+    }
 }
